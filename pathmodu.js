ee823bed2a731fd162c5ede01eda79b3e62d6c8d
myproject01 <myproject@01.com>
2022-10-08 14:10:44 +0530

    COMMIT))!FIRST

--- pathmodu.js
+++ pathmodu.js
@@ -2,7 +2,6 @@ const path = require("path")
 console.log(path.sep)
 const file=path.join('/content/','subfolder','text.txt')
 console.log(file)
-//
 console.log(path.basename(file))
 console.log(path.resolve(__dirname,'content','subfolder','text.txt'))
 
